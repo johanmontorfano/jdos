42e4875424d5856ac2126b85ec711dd91a142794
Johan Montorfano <me@johanmontorfano.com>
2025-02-12 15:06:47 +0100

    add: possibility to switch keyboard layouts from shell

--- system/kernel/src/shell/shell.c
+++ system/kernel/src/shell/shell.c
@@ -15,17 +15,30 @@ static void print_help(void)
     print("EXIT             Halt the CPU\n");
     print("SHELl            Print shell version\n");
     print("KB.LAY <code>    Changes keyboard layout\n");
+    print("KB.GLAY          Get all available layouts\n");
+}
+
+static void print_layouts(void)
+{
+    print("AZERTY           code: 0\n");
+    print("QWERTY           code: 1\n");
 }
 
 void on_line(char *str)
 {
-    if (!s_strcmp(str, "EXIT")) {
+    char **args = argparse(str);
+
+    if (!s_strcmp(args[0], "EXIT")) {
         print("CPU halted");
         asm volatile("hlt");
     }
-    if (!s_strcmp(str, "SHELL"))
+    if (!s_strcmp(args[0], "SHELL"))
         print_shell_info();
-    if (!s_strcmp(str, "HELP"))
+    if (!s_strcmp(args[0], "HELP"))
         print_help();
+    if (!s_strcmp(args[0], "KEY.GLAY"))
+        print_layouts();
+    if (!s_strcmp(args[0], "KEY.LAY"))
+        update_kb_layout(atoi(args[1]));
     print("> ");
 }
