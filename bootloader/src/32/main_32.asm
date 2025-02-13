4def2b09beb7c29a4d1896d863e0e8cfa87e083c
Johan Montorfano <me@johanmontorfano.com>
2025-02-13 17:31:26 +0100

    fix: increased bootloader read size

--- bootloader/src/32/main_32.asm
+++ bootloader/src/32/main_32.asm
@@ -26,7 +26,7 @@ load_kernel:
     call print_nl
 
     mov bx, KERNEL_OFFSET
-    mov dh, 32                  ; Kernel size
+    mov dh, 44                  ; Kernel size
     mov dl, [BOOT_DRIVE]
     call disk_load
     ret
