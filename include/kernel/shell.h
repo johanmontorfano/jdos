d4aed45923d425dfca559b0aab5971c2d269db42
Johan Montorfano <me@johanmontorfano.com>
2025-02-14 02:35:49 +0100

    refactor: moved  to include/kernel

--- include/kernel/shell.h
+++ include/kernel/shell.h
@@ -2,7 +2,11 @@
     #define KERNEL_SHELL
     #define SHELL_NAME "jdsh (johan's dumb shell)"
     #define SHELL_VER 0
+    #define DISKSHELL_NAME "DISKSHELL"
+    #define DISKSHELL_VER 0
 
+void diskshell();
 void on_line(char *str);
+void print_program_handle(char *name, int version);
 
 #endif
